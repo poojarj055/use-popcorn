4648ef64d2f40a4dca8dbebb0e4c3b5857f4ba1e
poojaraj <poojaraj@bidgely.com>
2024-04-21 17:14:31 +0530

    Updated at 5:10 pm on 21st april 2024

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+//import { handleRating } from './StarRating';
+//import StarRating from "./StarRating";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   {/* <StarRating
+  maxRating={5}
+  messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+ 
+/> */}
+<App/>
+
+   
+
+    
+  </React.StrictMode>
+);
+
+
